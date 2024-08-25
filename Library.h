b7da7fc65c868d605efe9d44094f77635f44ece3
Lilynguyen216 <165750572+Lilynguyen216@users.noreply.github.com>
2024-08-25 23:37:48 +0700

    Create Library.h

---
+++ Library.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <Windows.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <conio.h>
+#include <time.h>
+#include <iomanip>
+#include <fstream>
+#include <random>
+using namespace std;
